fa9365575d2b9850655051793b60b54a5315a543
Szymon Klarman <sklarman@users.noreply.github.com>
2020-03-04 14:12:10 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "Staple API demo",
+  "version": "1.0.0",
+  "description": "A knowledge graph app in 15 min",
+  "main": "demo.js",
+  "directories": {
+    "doc": "docs"
+  },
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/epistemik-co/staple-api-kg-demo.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/epistemik-co/staple-api-kg-demo/issues"
+  },
+  "homepage": "https://github.com/epistemik-co/staple-api-kg-demo#readme"
+}
