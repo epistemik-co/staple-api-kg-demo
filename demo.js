999cba801120af365e26091b2fa4e81af221e412
EPISTEMIK <43876616+epistemik-co@users.noreply.github.com>
2020-03-09 09:41:19 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -18,44 +18,40 @@ async function Demo() {
     // creating an instance of Staple API
 
     const stapleApi = await staple(ontology, config);
-    const schema = stapleApi.schema
-
-
+  
     // creating the list of all people and their names for FE indexing
 
     let people = []
 
     console.log("Fetching people's names...")
     await stapleApi.graphql('{ Person { _id label } }').then((response) => {
-        pips = response.data.Person;
-        pips.forEach(element => {
+        response.data.Person.forEach(element => {
             people.push({id: element._id, text:element.label})
         });
-        console.log("All fetched...")
+        console.log("...all fetched!")
         });
 
 
-    // exnabling FE, Staple API and people names via express server
+    // exnabling FE, Staple API and people endpoint via express server
 
     const app = express();
+    
+    app.use(express.static("docs"))
 
     app.get('/people', function (req, res) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.send(people)
       })
 
-    const path = "/graphql"
     const server = new ApolloServer({
-        schema
+        stapleApi.schema
     });
 
-    server.applyMiddleware({ app, path });
+    server.applyMiddleware({ app, "/graphql" });
 
     app.listen({ port: 5000 }, () =>
         console.log("🚀 Server ready")
     );
-
-    app.use(express.static("docs"))
 }
 
 Demo()
